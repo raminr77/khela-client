c5b28b06bd8e849edebc3d2520c4da21b7326b13
Ramin <Ramin.rezaei77@gmail.com>
2025-03-17 23:50:34 +0100

    feat: Add @/ for paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
-import './styles/tailwind.css';
-import './styles/main.scss';
+import '@/styles/tailwind.css';
+import '@/styles/main.scss';
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
