2789f6d1d2191085078552d9fec04dbb2b093994
Ramin Rezaei <rare@boozt.com>
2025-03-17 17:35:03 +0100

    feat: Move to Scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
-import './styles/main.css';
+import './styles/main.scss';
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
