b5e9be9076909313590a11887e9b184a90b4c429
Ramin <Ramin.rezaei77@gmail.com>
2025-03-19 00:42:59 +0100

    feat: Add PWA Configs

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-function registerServiceWorker() {
-  if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(() => {
-      console.log('Service Worker registration successful.');
-    });
-  }
-}
-
-registerServiceWorker();
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import path from 'path';
@@ -6,7 +7,20 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script-defer',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,jpeg,jpg,png,svg}']
+      },
+      devOptions: {
+        enabled: false
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
