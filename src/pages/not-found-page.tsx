17f5655bbd699410df8baf61f7726792cdf401c6
Ramin <Ramin.rezaei77@gmail.com>
2025-03-21 20:34:49 +0100

    feat: Add 404 Page

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -9,23 +9,38 @@ export function NotFoundPage() {
     <main
       className={clsx(
         'w-full h-screen overflow-hidden flex flex-col items-center justify-center',
-        animator({ name: 'fadeIn' })
+        animator({ name: 'fadeIn', speed: 'faster' })
       )}
     >
       <img
-        className={clsx('mb-7', animator({ name: 'bounce' }))}
+        className={clsx('mb-7', animator({ name: 'jackInTheBox' }))}
         src='/images/404-poop.png'
         alt='404 Page Not Found'
         height={100}
         width={100}
       />
-      <h1 className='font-title text-2xl text-amber-600'>Something smells off…</h1>
-      <p className='text-center mt-4'>This page is gone!</p>
-      <p className='text-center mt-2'>Just like when you’re late for the toilet.</p>
+      <h1
+        className={clsx(
+          'font-title text-2xl text-amber-600',
+          animator({ name: 'fadeInUp' })
+        )}
+      >
+        Something smells off…
+      </h1>
 
-      <Button asChild variant='outline' className='mt-6'>
-        <Link to={APP_ROUTES.main}>Back to Khela!</Link>
-      </Button>
+      <div
+        className={clsx(
+          'text-center mt-4 flex flex-col items-center justify-center',
+          animator({ name: 'fadeIn' })
+        )}
+      >
+        <p>This page is gone!</p>
+        <p className='mt-2'>Just like when you’re late for the toilet.</p>
+
+        <Button asChild variant='outline' className='mt-6'>
+          <Link to={APP_ROUTES.main}>Back to Khela!</Link>
+        </Button>
+      </div>
     </main>
   );
 }
