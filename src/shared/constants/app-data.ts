e9de8017f821d1ef1608b5306148f5fd1b78644b
Ramin <Ramin.rezaei77@gmail.com>
2025-03-24 01:49:41 +0100

    feat: Add email for contact us

--- src/shared/constants/app-data.ts
+++ src/shared/constants/app-data.ts
@@ -1,6 +1,7 @@
 export const APP_DATA = {
   name: 'Khela',
   version: '1.0.0',
+  email: 'info@khela.ir',
   logo: '/images/logo.png',
   description: 'Khela is a fun project for sharing your pee and poop with friends!'
 } as const;
