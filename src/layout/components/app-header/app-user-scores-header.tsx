0cfa535cb44b8465917e7f3ef7ced5ee84362663
Ramin <Ramin.rezaei77@gmail.com>
2025-03-30 03:02:48 +0200

    feat: Add new score image

--- src/layout/components/app-header/app-user-scores-header.tsx
+++ src/layout/components/app-header/app-user-scores-header.tsx
@@ -1,11 +1,16 @@
-import { SparklesIcon } from 'lucide-react';
-
 import { animator } from '@/shared/helpers';
 
 export function AppUserScoresHeader() {
   return (
     <div className='flex items-center gap-1 text-amber-600'>
-      <SparklesIcon className={animator({ name: 'jello' })} />
+      <img
+        width={20}
+        alt='Score'
+        height={20}
+        loading='lazy'
+        src='/images/star.png'
+        className={animator({ name: 'jello' })}
+      />
       <span className={animator({ name: 'fadeIn' })}>100</span>
     </div>
   );
